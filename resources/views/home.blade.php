27897f445d1ee63ae8e0a1012aec3f9e225691b1
Hitsukaya Valentaizar <87619517+Hitsukaya@users.noreply.github.com>
2025-08-27 15:24:21 +0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,24 @@
     </div>
 
     <div class="container mx-auto p-6">
-        @livewire('multi-kml-uploader')
+        @livewire('kml-to-csv')
+    </div>
+
+    <div class="container mx-auto p-6">
+        @livewire('csv-to-kml')
+    </div>
+	
+	<div class="container mx-auto p-6">
+        @livewire('geo-json-to-csv')
+    </div>
+	
+	<div class="container mx-auto p-6">
+        @livewire('geojson-to-kml')
+    </div>
+	
+
+	
+	<div class="container mx-auto p-6">
+        @livewire('ua-kml-to-csv')
     </div>
 </x-layouts.app>
